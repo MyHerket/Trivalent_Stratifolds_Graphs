d5c3dc7e97883da166a9ae84326cae4389f0b08c
MyHerket <41802492+MyHerket@users.noreply.github.com>
2020-07-16 19:42:11 -0600

    implementación string

--- AHU_Algorithm.py
+++ AHU_Algorithm.py
@@ -27,7 +27,6 @@ def clean_leaf(graph, leaf):
 		graph.remove_node(leaf)
 	return
 
-
 def center(graph):
 	G = graph.copy()
 	#paths = []
@@ -45,11 +44,42 @@ def center(graph):
 	#print("El camino mas largo fue: ", l_path, " y el centro es ", l_path[center_i])
 	return l_path[center_i]
 
+def four(name):
+	return int(name, 4)
 
+def renaming(graph, source, father):
+	path_list = []
+	if father == -1:
+		a = '0'
+		b = '1'
+	else:
+		w = graph[source][father][0]['weight']
+		if w == 1: 
+			a = '0'
+			b = '1'
+		elif w == 2: 
+			a = '2'
+			b = '3'
+	
+	if graph.degree(source) == 1 and father != -1:
+		return a+b
+	else:
+		neighbor = list(graph.neighbors(source))
+		if father != -1: neighbor.remove(father)
+		for n in neighbor:
+			paths = renaming(graph, n, source)
+			path_list.append(paths)
+		path_list.sort(key = four)
+		string = a
+		for p in path_list: string+=p
+		return string + b
 
 
 G = b111()
 G = G.O1(0)
 G= G.O1(0)
-
-print(center(G))
+#print(G[1]['a'][0]['weight'])
+X = center(G)
+print(X)
+R = renaming(G, X, -1)
+print(R)
