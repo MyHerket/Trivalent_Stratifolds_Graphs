c9286ff8cdcacdacea91b4b0885ca64b33a903c9
jesus <Jesus Rodriguez Viorato>
2020-08-04 12:00:24 -0500

    - Simplicagtion of the dfs algorithm.  It returns now the largest path instead of the whole list of paths.
     - Center function now computes the center by running the dfs algorithm just twice. We avoided the use of G.leafs(), cloning, and graph's modifications.

--- AHU_Algorithm.py
+++ AHU_Algorithm.py
@@ -5,22 +5,23 @@ def dfs(graph, source, father):
 	"""
 	Deep-first Search implementation in the graph. It is a recursive function that maps all the paths from one node to another.
 	"""
-	path_list = []
 	if graph.degree(source) == 1 and father != -1:
-		return [[source, 1]]
+		return [source]
 	else:
 		neighbor = list(graph.neighbors(source))
 		if father != -1: neighbor.remove(father)
+		longestPath = []
+		length = 0
 		for n in neighbor:
-			paths = dfs(graph, n, source)
-			for p in paths:
-				p[-1] += 1
-				path_list.append([source]+p)
-		return path_list
+			path = dfs(graph, n, source)
+			if(len(path) > length ):
+				length = len(path)
+				longestPath = path
+		return [source]+longestPath
 
 def clean_leaf(graph, leaf):
 	"""
-	This function helps eliminates a leaf and the edges conected to that leaf. 
+	This function helps eliminates a leaf and the edges conected to that leaf.
 	Also ereases all the nodes that turns into a leaf when the first one is eliminated.
 	"""
 	neighbor = list(graph.neighbors(leaf))[0]
@@ -30,7 +31,7 @@ def clean_leaf(graph, leaf):
 	elif graph.degree(neighbor) == 2:
 		graph.remove_node(leaf)
 		clean_leaf(graph, neighbor)
-	else: 
+	else:
 		graph.remove_node(leaf)
 	return
 
@@ -39,23 +40,18 @@ def center(graph):
 	Returns the name of the node that is the center of a graph
 	"""
 	G = graph.copy()
-	largest = 0
-	shortest = 100000000
-	while len(G.leaves()) > 0:
-		node = list(G.leaves())[0]
-		dfs_list = dfs(G, node, -1)
-		for p in dfs_list:
-			if p[-1] > largest:
-				largest = p[-1]
-				l_path = p
-			if p[-1] < shortest: 
-				shortest = p[-1]
-		clean_leaf(G, node)
-	center_i = int((l_path[-1]-1)/2)
-	graph.root = l_path[center_i]
+	node = list(G.nodes())[0]
+
+	longestPath = dfs(G, node, -1)
+	longestPath = dfs(G, longestPath[-1], -1)
+
+	center_i = int((len(longestPath)-1)/2)
+
+	graph.root = longestPath[center_i]
 	#graph.tag[3] = shortest
-	#graph.tag[4] = largest
-	return l_path[center_i]
+
+	return longestPath[center_i]
+
 
 def four(name):
 	"""
@@ -80,13 +76,13 @@ def rooted_tree(graph, source, father):
 		b = '1'
 	else:
 		w = graph[source][father][0]['weight']
-		if w == 1: 
+		if w == 1:
 			a = '0'
 			b = '1'
-		elif w == 2: 
+		elif w == 2:
 			a = '2'
 			b = '3'
-	
+
 	if graph.degree(source) == 1:
 		return a+b
 	else:
@@ -94,12 +90,26 @@ def rooted_tree(graph, source, father):
 		if father != -1: neighbor.remove(father)
 		for n in neighbor:
 			paths = rooted_tree(graph, n, source)
-			graph.nodes[n]['word'] = paths #Esta línea guarda la palabra generada para el nodo en el nodo 
+			graph.nodes[n]['word'] = paths #Esta línea guarda la palabra generada para el nodo en el nodo
 			path_list.append(paths)
 		path_list.sort(key = four)
 		string = a
 		for p in path_list: string+=p
 		return string + b
 
+def test():
+	G2=strat_graph()
+	#edgs=[(1,'a',2),(1,'b',2),(2,'a'),(3,'b')]
+	edgs=[(0,'a'),(0, 'b'),(1,'a',2), (2,'b'),(3,'b')]
+	G2.addEdg(edgs)
+	G2.tag = 'example'
+	G2.draw(trivalent=True)
+	longestPath = dfs(G2, 3, -1)
+	longestPath = dfs(G2, longestPath[-1],-1)
+	print(longestPath)
 
- 
+	center(G2,True)
+	print(G2.root)
+
+if __name__ == "__main__":
+	test()
